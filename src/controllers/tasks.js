d154b8ed4b82f9306439545b7d39e7247cf68360
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-22 13:24:15 +0300

    update getAllTasks, createTask controllers with Task model usage

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,12 +1,31 @@
 const Task = require('../models/task');
 
-const getAllTasks = (req, res) => {
-  res.status(200).send('get all tasks');
+const getAllTasks = async (req, res) => {
+  try {
+    // IMPORTANT!!! Technically Models QUERIES DOES NOT RETURN Promise
+    // BUT they HAVE "THEN" METHOD implemented which is convenient for us
+    // as we now can use "async-await" combo. One of the available "query"-methods
+    // is "find". It accepts a "filter object". For example:
+    // - Task.find({}) will resolve to ALL TASKS from the "tasks" collection in DB;
+    // - Task.find({ completed: tru }) will resolve to ALL COMPLETED TASKS;
+    // const tasks = await Task.find({ completed: true });
+    const tasks = await Task.find({});
+
+    return res.status(200).json({ tasks });
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    // here we use what was added to DB
+    const task = await Task.create(req.body);
+
+    return res.status(201).json({ task });
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
 };
 
 const getTask = (req, res) => {
